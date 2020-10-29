a70d99062ccf5633f0e6edc8efcc84f57f840c31
Vajk <vajkone18@gmail.com>
2020-10-29 14:29:51 +0100

    Added NoteTaking.kt and NoteListing.kt

---
+++ app/src/main/java/com/dcs/productivityapp/Activities/NoteTaking.kt
@@ -0,0 +1,12 @@
+package com.dcs.productivityapp.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dcs.productivityapp.R
+
+class NoteTaking : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_taking)
+    }
+}
