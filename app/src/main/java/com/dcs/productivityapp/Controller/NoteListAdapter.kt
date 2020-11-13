6ec1fa6ca7e344d75ca8d77bbdc7d904e1ce741a
Vajk <vajkone18@gmail.com>
2020-11-13 17:55:35 +0100

    Added method for deleting notes

--- app/src/main/java/com/dcs/productivityapp/Controller/NoteListAdapter.kt
+++ app/src/main/java/com/dcs/productivityapp/Controller/NoteListAdapter.kt
@@ -13,6 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dcs.productivityapp.Activities.NoteTaking
 import com.dcs.productivityapp.Model.Note
 import com.dcs.productivityapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
 
 class NoteListAdapter(private var list: MutableList<Note>, private val context: Context):
     androidx.recyclerview.widget.RecyclerView.Adapter<NoteListAdapter.ViewHolder>() {
@@ -26,6 +34,7 @@ class NoteListAdapter(private var list: MutableList<Note>, private val context: 
             val noteDate: TextView =itemView.findViewById(R.id.noteDate)
             val noteDeleteButton: TextView = itemView.findViewById(R.id.noteDeleteButton)
 
+
             noteTitle.text=note.noteTitle
             noteLabel.text=note.noteLabel
             noteDate.text=note.noteDate
@@ -37,10 +46,11 @@ class NoteListAdapter(private var list: MutableList<Note>, private val context: 
                 builder.setMessage("Are you sure you want to Delete?")
                     .setTitle("Warning!")
                     .setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, wich ->
-                       TODO("Implement cloud remove function")
-//                        val a = adapterPosition
-//                        list.removeAt(a)
-//                        notifyItemRemoved(adapterPosition)
+                        deleteNote(note)
+                        val a = adapterPosition
+                        list.removeAt(a)
+                        notifyItemRemoved(adapterPosition)
+
                     })
                     .setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, wich ->
                         // User cancelled the dialog
@@ -60,6 +70,30 @@ class NoteListAdapter(private var list: MutableList<Note>, private val context: 
 
             }
 
+        }
+
+        private fun deleteNote(note: Note)= CoroutineScope(Dispatchers.IO).launch {
+            var currentUser= FirebaseAuth.getInstance().currentUser
+
+            val notesDocRef= Firebase.firestore.collection("users")
+                .document(currentUser!!.uid)
+                .collection("notes")
+
+
+            val noteQuery = notesDocRef
+                .whereEqualTo("id",note.id)
+                .get()
+                .await()
+
+            if (noteQuery.documents.isNotEmpty()){
+               for (doc in noteQuery){
+                   try {
+                       notesDocRef.document(doc.id).delete().await()
+                   }catch (e:Exception){
+                       Log.e("Error: ",e.message)
+                   }
+               }
+            }
         }
 
     }
