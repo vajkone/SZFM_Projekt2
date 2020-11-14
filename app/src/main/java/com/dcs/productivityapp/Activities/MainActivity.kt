fd504ade0f2f6ddc68befd89e7aae81e6ee0d050
noramozes <43684374+noramozes@users.noreply.github.com>
2020-11-14 13:47:40 +0100

    Implemented loading new activity and logging out when menu items are clicked

--- app/src/main/java/com/dcs/productivityapp/Activities/MainActivity.kt
+++ app/src/main/java/com/dcs/productivityapp/Activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.dcs.productivityapp.Model.User
@@ -11,6 +13,7 @@ import com.dcs.productivityapp.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.*
 import com.google.firebase.database.ValueEventListener as ValueEventListener1
 
 class MainActivity : AppCompatActivity() {
@@ -24,10 +27,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        imageMenu.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
 
         var user = FirebaseAuth.getInstance().currentUser
         var dbref = FirebaseDatabase.getInstance().getReference("users")
@@ -49,20 +48,43 @@ class MainActivity : AppCompatActivity() {
 
                     userName.text = name
                     userEmail.text = email
+
+                    usersName.text = name
+                    usersEmail.text = email
                 }
             }
         })
 
-        logoutBtn.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            val i=Intent(this,LoginActivity::class.java)
-            i.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(i)
+        imageMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
 
-        gotoNotesBtn.setOnClickListener {
-            val i=Intent(this,NoteListing::class.java)
-            startActivity(i)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }
+                R.id.notes -> {
+                    val i=Intent(this,NoteListing::class.java)
+                    startActivity(i)
+                }
+                R.id.todo -> {
+
+                }
+                R.id.habits -> {
+
+                }
+                R.id.contact -> {
+
+                }
+                R.id.logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    val i=Intent(this,LoginActivity::class.java)
+                    i.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(i)
+                }
+            }
+            true
         }
 
     }
