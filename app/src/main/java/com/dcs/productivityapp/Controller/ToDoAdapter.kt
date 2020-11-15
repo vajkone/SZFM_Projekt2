37a22be97c705f7bfcada8ceb49b184300fc5cc0
kissmate3 <kissmate222@gmail.com>
2020-11-15 10:35:09 +0100

    Added ToDoAdapter.kt to Controller

---
+++ app/src/main/java/com/dcs/productivityapp/Controller/ToDoAdapter.kt
@@ -0,0 +1,11 @@
+package com.dcs.productivityapp.Controller
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.dcs.productivityapp.Activities.UpdateAndDelete
+import com.dcs.productivityapp.Model.ToDoModel
+import com.dcs.productivityapp.R
+
