e27a1ac3f104bf50842acbd38dfc4a9ed1a55c1b
kissmate3 <kissmate222@gmail.com>
2020-12-01 21:45:32 +0100

    Fix TODO

--- app/src/main/java/com/dcs/productivityapp/Activities/TodoListing.kt
+++ app/src/main/java/com/dcs/productivityapp/Activities/TodoListing.kt
@@ -1,1 +1,92 @@
+package com.dcs.productivityapp.Activities
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dcs.productivityapp.Controller.ToDoAdapter
+
+class TodoListing : AppCompatActivity() {
+
+
+    private var recentTodoId: String?=""
+    private var layoutManager2: RecyclerView.LayoutManager? = null
+    private var ToDoAdapter: ToDoAdapter? = null
+    private var todoList: MutableList<ToDoModel>? = null
+    private var todoListItems: ArrayList<ToDoModel>? = null
+
+    private var currentUser= FirebaseAuth.getInstance().currentUser
+
+    private val todosDocRef= Firebase.firestore.collection("users")
+        .document(currentUser!!.uid)
+        .collection("todos")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_listing)
+
+        layoutManager2= LinearLayoutManager(this)
+        todoList= ArrayList()
+        todoListItems= ArrayList()
+
+        ToDoAdapter= ToDoAdapter(todoListItems!!,this)
+        getTodos()
+
+        for (todos in todoListItems!!){
+            Log.d("In: ",todos.itemDataText!!)
+        }
+
+        item_listView.layoutManager=layoutManager2
+        item_listView.adapter= ToDoAdapter
+
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+
+        fab.setOnClickListener { view ->
+            val alertDialog = AlertDialog.Builder(this)
+            val textEditText = EditText(this)
+            alertDialog.setMessage("Add TODO item")
+            alertDialog.setTitle("Enter To Do item")
+            alertDialog.setView(textEditText)
+            alertDialog.setPositiveButton("Add") { dialog, i ->
+                createTodo(textEditText)
+            }
+            alertDialog.show()
+        }
+
+
+    }
+
+    private fun createTodo(s: EditText) {
+
+        var todo = ToDoModel()
+
+
+        todo.itemDataText = s.text.toString()
+        todo.ID = todosDocRef.document().id
+        todo.done = false
+        Toast.makeText(this, todo.itemDataText, Toast.LENGTH_LONG).show()
+
+        Log.d("Note id: ",todo.ID!!)
+
+        if (todo.itemDataText == "") {
+            Toast.makeText(applicationContext, "Please enter a title", Toast.LENGTH_SHORT).show()
+
+        } else {
+            saveTodo(todo)
+            todoListItems?.clear()
+            getTodos()
+            ToDoAdapter!!.notifyDataSetChanged()
+
+        }
+    }
--- app/src/main/java/com/dcs/productivityapp/Controller/ToDoAdapter.kt
+++ app/src/main/java/com/dcs/productivityapp/Controller/ToDoAdapter.kt
@@ -1,1 +1,77 @@
+package com.dcs.productivityapp.Controller
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Build.ID
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.dcs.productivityapp.Activities.TodoListing
+
+class ToDoAdapter(private var list: MutableList<ToDoModel>, private val context: Context):
+    androidx.recyclerview.widget.RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
+
+    private var currentUser= FirebaseAuth.getInstance().currentUser
+    private val todosDocRef= Firebase.firestore.collection("users")
+        .document(currentUser!!.uid)
+        .collection("todos")
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+
+        fun bindTodo(todo: ToDoModel){
+
+            val todoTitle = itemView.findViewById(R.id.item_textView) as TextView
+            val todoDeleteButton: ImageButton =itemView.findViewById(R.id.close) as ImageButton
+            val isDone: CheckBox =itemView.findViewById(R.id.checkbox) as CheckBox
+
+            todoTitle.text=todo.itemDataText
+            isDone.isChecked = todo.done!!
+
+            todoDeleteButton.setOnClickListener {
+
+                val builder = AlertDialog.Builder(context)
+                builder.setMessage("Are you sure you want to Delete?")
+                    .setTitle("Warning!")
+                    .setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, wich ->
+                        deleteTodo(todo)
+                        val a = adapterPosition
+                        list.removeAt(a)
+                        notifyItemRemoved(adapterPosition)
+
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, wich ->
+                        // User cancelled the dialog
+                    })
+                    .show()
+
+                builder.create()
+
+            }
+
+            isDone.setOnClickListener{
+                val todo2=updatedTodo(todo)
+                updateTodo(todo2!!)
+
+            }
+
+        }
+
+        private fun deleteTodo(todo: ToDoModel)= CoroutineScope(Dispatchers.IO).launch {
+            var currentUser= FirebaseAuth.getInstance().currentUser
+
+            val notesDocRef= Firebase.firestore.collection("users")
+                .document(currentUser!!.uid)
+                .collection("todos")
+
+
+            val noteQuery = notesDocRef
+                .whereEqualTo("id",todo.ID)
+                .get()
+                .await()
 
