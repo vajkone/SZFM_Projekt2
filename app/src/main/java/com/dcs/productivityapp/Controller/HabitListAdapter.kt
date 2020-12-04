8fc5fdf570bcb064a9dfa2bdc3766332afe809d1
foldesizoltan14 <43464693+foldesizoltan14@users.noreply.github.com>
2020-12-04 20:33:27 +0100

    Expended and finished HabitListAdapter

--- app/src/main/java/com/dcs/productivityapp/Controller/HabitListAdapter.kt
+++ app/src/main/java/com/dcs/productivityapp/Controller/HabitListAdapter.kt
@@ -84,14 +84,14 @@ class HabitListAdapter (private var list: MutableList<Habits>, private val conte
                 val builder = AlertDialog.Builder(context)
                 builder.setMessage("Are you sure you want to Delete?")
                     .setTitle("Warning!")
-                    .setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, wich ->
+                    .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, wich ->
                         deleteHabit(habit)
                         val a = adapterPosition
                         list.removeAt(a)
                         notifyItemRemoved(adapterPosition)
 
                     })
-                    .setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, wich ->
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, wich ->
                         // User cancelled the dialog
                     })
                     .show()
@@ -99,3 +99,141 @@ class HabitListAdapter (private var list: MutableList<Habits>, private val conte
                 builder.create()
 
             }
+
+            isDone.setOnClickListener {
+                val habit2=updatedHabit(
+                    habit)
+                updateHabit(habit2!!)
+            }
+            itemView.setOnClickListener {
+
+                val intent = Intent(context, HabitSum::class.java)
+                intent.putExtra("habitId",habit.ID)
+
+                context.startActivity(intent)
+
+            }
+        }
+    }
+
+    private fun deleteHabit(habit: Habits)= CoroutineScope(Dispatchers.IO).launch {
+        var currentUser= FirebaseAuth.getInstance().currentUser
+
+        val notesDocRef= Firebase.firestore.collection("users")
+            .document(currentUser!!.uid)
+            .collection("habits")
+
+
+        val noteQuery = notesDocRef
+            .whereEqualTo("id",habit.ID)
+            .get()
+            .await()
+
+        if (noteQuery.documents.isNotEmpty()){
+            for (doc in noteQuery){
+                try {
+                    notesDocRef.document(doc.id).delete().await()
+
+                }catch (e: Exception){
+                    Log.e("Error: ",e.message)
+                }
+            }
+        }
+    }
+
+
+
+    private fun updatedHabit(habitin : Habits? ): Habits? {
+
+        var habit = Habits()
+        habit.itemDataText = habitin?.itemDataText
+        habit.done = !habitin?.done!!
+        habit.done2 = habitin.done2!!
+        habit.done3 = habitin.done3!!
+        habit.ID = habitin.ID
+        habit.checkDate = habitin.checkDate
+        habit.days = habitin.days+1
+        habit.creationDate = habitin.creationDate
+        Log.d("SUCCES",habitin.done.toString())
+        Log.d("SUCCES",habit.done.toString())
+
+        return habit
+
+
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun slideHabbit(habitin : Habits? ): Habits? {
+        var seged : Boolean = false
+        val today = LocalDate.now()
+        val form = "yyyy.MM.dd"
+
+        val formattedToday = today.format(DateTimeFormatter.ofPattern(form))
+        var habit = Habits()
+        habit.itemDataText = habitin?.itemDataText
+        seged = habitin!!.done2!!
+
+        habit.done2 = habitin!!.done!!
+        habit.creationDate = habitin.creationDate
+        habit.done = false
+        habit.done3 = seged!!
+        habit.ID = habitin!!.ID
+        habit.checkDate = formattedToday
+        habit.days = habitin.days
+
+
+        Log.d("SUCCES",habitin.done.toString())
+        Log.d("SUCCES",habit.done.toString())
+
+        return habit
+
+
+
+    }
+
+
+    private fun updateHabit(habit:Habits)= CoroutineScope(Dispatchers.IO).launch {
+
+        val noteQuery=habitsDocRef
+            .whereEqualTo("id",habit.ID)
+            .get()
+            .await()
+
+        if (noteQuery.documents.isNotEmpty()){
+            for (doc in noteQuery){
+                try {
+                    Log.d("SUCCES",habit.done.toString())
+                    habitsDocRef.document(doc.id).set(habit,SetOptions.merge())
+
+                    Log.d("SUCCES",doc.id)
+                    Log.d("SUCCES",habit.ID)
+                    Log.d("SUCCES",habit.done.toString())
+
+
+                }catch (e:Exception){
+                    Log.e("error: ",e.message)
+                }
+            }
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitListAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.row_itemslayout_habit,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return  list.size
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBindViewHolder(holder: HabitListAdapter.ViewHolder, position: Int) {
+        holder.bindHabit(list[position])
+    }
+
+
+}
