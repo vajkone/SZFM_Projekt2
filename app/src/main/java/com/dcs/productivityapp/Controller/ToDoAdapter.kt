5c419c7a51414f6004db025992b2fe16e64a3ee8
kissmate3 <kissmate222@gmail.com>
2020-12-04 21:20:09 +0100

    Fix ToDoAdapter.kt

--- app/src/main/java/com/dcs/productivityapp/Controller/ToDoAdapter.kt
+++ app/src/main/java/com/dcs/productivityapp/Controller/ToDoAdapter.kt
@@ -1,81 +1,94 @@
 package com.dcs.productivityapp.Controller
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.os.Build.ID
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.dcs.productivityapp.Activities.TodoListing
 import com.dcs.productivityapp.Activities.UpdateAndDelete
 import com.dcs.productivityapp.Model.ToDoModel
 import com.dcs.productivityapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.row_itemslayout.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
 
-class ToDoAdapter(context: Context,toDoList:MutableList<ToDoModel>) : BaseAdapter() {
+class ToDoAdapter(private var list: MutableList<ToDoModel>, private val context: Context):
+    androidx.recyclerview.widget.RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
 
-    private val inflater:LayoutInflater= LayoutInflater.from(context)
-    private var itemList=toDoList
-    private var updateAndDelete: UpdateAndDelete = context as UpdateAndDelete
+    private var currentUser= FirebaseAuth.getInstance().currentUser
+    private val todosDocRef= Firebase.firestore.collection("users")
+        .document(currentUser!!.uid)
+        .collection("todos")
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val UID: String? = itemList.get(p0).UID as String?
-        val itemTextData = itemList.get(p0).itemDataText as String?
-        val done = if  (itemList.get(p0).done != null) itemList.get(p0).done as Boolean?
-        else false
 
-        val view: View
-        val viewHolder: ListViewHolder
 
-        if (p1 == null) {
-            view = inflater.inflate(R.layout.row_itemslayout, p2, false)
-            viewHolder = ListViewHolder(view)
-            view.tag = viewHolder
-        } else {
-            view = p1
-            viewHolder = view.tag as ListViewHolder
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+
+        fun bindTodo(todo: ToDoModel){
+
+            val todoTitle = itemView.findViewById(R.id.item_textView) as TextView
+            val todoDeleteButton: ImageButton =itemView.findViewById(R.id.close) as ImageButton
+            val isDone: CheckBox =itemView.findViewById(R.id.checkbox) as CheckBox
+
+            todoTitle.text=todo.itemDataText
+            isDone.isChecked = todo.done!!
+
+            todoDeleteButton.setOnClickListener {
+
+                val builder = AlertDialog.Builder(context)
+                builder.setMessage("Are you sure you want to Delete?")
+                    .setTitle("Warning!")
+                    .setPositiveButton("Yes", DialogInterface.OnClickListener{ dialog, wich ->
+                        deleteTodo(todo)
+                        val a = adapterPosition
+                        list.removeAt(a)
+                        notifyItemRemoved(adapterPosition)
+
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, wich ->
+                        // User cancelled the dialog
+                    })
+                    .show()
+
+                builder.create()
+
+            }
+
+            isDone.setOnClickListener{
+                val todo2=updatedTodo(todo)
+                updateTodo(todo2!!)
+
+            }
+
         }
 
-        viewHolder.textLabel.text = itemTextData
-        viewHolder.isDone.isChecked = done!!
+        private fun deleteTodo(todo: ToDoModel)= CoroutineScope(Dispatchers.IO).launch {
+            var currentUser= FirebaseAuth.getInstance().currentUser
 
-        viewHolder.isDone.setOnClickListener {
-            updateAndDelete.modifyItem(UID!!, !done!!)
-        }
-
-        viewHolder.isDeleted.setOnClickListener{
-            updateAndDelete.onItemDelete(UID!!)
-        }
-
-        return view
-    }
+            val notesDocRef= Firebase.firestore.collection("users")
+                .document(currentUser!!.uid)
+                .collection("todos")
 
 
-    override fun getCount(): Int {
-        return itemList.size
-    }
+            val noteQuery = notesDocRef
+                .whereEqualTo("id",todo.ID)
+                .get()
+                .await()
 
-    override fun getItem(p0: Int): Any {
-        return itemList.get(p0)
-    }
-
-    override fun getItemId(p0: Int): Long {
-        return p0.toLong()
-    }
-
-
-
-
-
-
-
-
-
-
-
-}
-private class ListViewHolder(row:View?) {
-    val textLabel: TextView=row!!.findViewById(R.id.item_textView) as TextView
-    val isDone: CheckBox =row!!.findViewById(R.id.checkbox) as CheckBox
-    val isDeleted: Button=row!!.findViewById(R.id.close) as Button
-}
 
 
 
